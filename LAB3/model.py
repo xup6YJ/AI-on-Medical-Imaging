1dff242d3d8a063075577debd0b6d20de4a31a1f
xup6yj <xup6yjxup6yj9619@gmail.com>
2023-05-04 19:52:13 +0800

    model_v3

--- LAB3/model.py
+++ LAB3/model.py
@@ -1,5 +1,4 @@
 
- 
 
 import torch.nn as nn
 import torchvision.models
@@ -19,6 +18,21 @@ class ConvRelu(nn.Module):
 
     def forward(self, x):
         return(self.convrelu(x))
+    
+class ConvReluDrop(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size = 3, stride = 1, padding = 1):
+        super(ConvRelu, self).__init__()
+
+        self.convrelu = nn.Sequential(
+            nn.Conv3d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride = stride, padding=padding),
+            nn.BatchNorm3d(num_features=out_channels),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5)
+        )
+
+    def forward(self, x):
+        return(self.convrelu(x))
+    
 
 class ConvSigmoid(nn.Module):
   def __init__(self, in_channels, out_channels, kernel_size = 3, padding = 1):
@@ -88,13 +102,8 @@ class FVBlock(nn.Module):
         self.in_channels = in_channels
         self.channel_att = SEBlock(self.in_channels, reduction_ratio=2)
         self.spatial_att = Spatial_block(self.in_channels)
-<<<<<<< HEAD
-        # self.conv1 = ConvRelu(in_channels = self.in_channels, out_channels = self.in_channels, kernel_size = 1, padding = 0)
-        self.conv1 = nn.Conv3d(in_channels=self.in_channels, out_channels=self.in_channels, kernel_size=1, padding=0)
-=======
         self.conv1 = nn.Conv3d(in_channels=self.in_channels, out_channels=self.in_channels, kernel_size=1, padding=0)
         # self.conv1 = ConvRelu(in_channels = self.in_channels, out_channels = self.in_channels, kernel_size = 1, padding = 0)
->>>>>>> 27376ab (train_v1 conv1 update)
         self.conv2 = ConvRelu(in_channels = int(self.in_channels*3), out_channels = self.in_channels, kernel_size = 3, padding = 1)
 
     def forward(self, x):
@@ -204,8 +213,6 @@ class encoder(nn.Module):
         return x4, down_sampling_feature
 
 
-
-
 # if __name__ == '__main__':
 #     inputs = torch.randn(1, 1, 16, 128, 128)
 #     inputs = inputs.cuda()
@@ -248,16 +255,17 @@ class decoder(nn.Module):
         # l4 output shape:  torch.Size([1, 512, 2, 16, 16])
 
         #l1
-        self.deconv1 = deconvblock(512, 512)
-        self.conv1 = ConvRelu(in_channels = 512*2, out_channels = 256, kernel_size = 3, padding = 1)
+        self.deconv1 = deconvblock(512, 256)
+        self.conv1 = ConvRelu(in_channels = 256*3, out_channels = 256, kernel_size = 3, padding = 1)
+        # self.conv1 = ConvRelu(in_channels = 256, out_channels = 256, kernel_size = 3, padding = 1)
 
         #l2
-        self.deconv2 = deconvblock(256, 256) #256 + 512 -> 128
-        self.conv2 = ConvRelu(in_channels = 512, out_channels = 128, kernel_size = 3, padding = 1)
+        self.deconv2 = deconvblock(256, 128) #256 + 512 -> 128
+        self.conv2 = ConvRelu(in_channels = 128*3, out_channels = 128, kernel_size = 3, padding = 1)
 
         #l3
-        self.deconv3 = deconvblock(128, 128) # 128 + 
-        self.conv3 = ConvRelu(in_channels = 256, out_channels = 64, kernel_size = 3, padding = 1)
+        self.deconv3 = deconvblock(128, 64) # 128 + 
+        self.conv3 = ConvRelu(in_channels = 64*3, out_channels = 64, kernel_size = 3, padding = 1)
 
         self.conv4 = ConvRelu(in_channels = 64, out_channels = 1, kernel_size = 3, padding = 1)
 
@@ -282,6 +290,71 @@ class decoder(nn.Module):
 
         return x4
 
+class ds_decoder(nn.Module):
+    def __init__(self, out_channels):
+        super(ds_decoder, self).__init__()
+        
+        # l1 output shape:  torch.Size([1, 128, 16, 128, 128])
+        # l2 output shape:  torch.Size([1, 256, 8, 64, 64])
+        # l3 output shape:  torch.Size([1, 512, 4, 32, 32])
+
+        # l4 output shape:  torch.Size([1, 512, 2, 16, 16])
+
+        #l1
+        self.deconv1 = deconvblock(512, 256)
+        self.conv1 = ConvReluDrop(in_channels = 256*3, out_channels = 256, kernel_size = 3, padding = 1)
+        self.conv2 = ConvRelu(in_channels = 256, out_channels = 256, kernel_size = 3, padding = 1)
+        #l1-2
+        self.upsample1 = nn.Upsample(scale_factor=4, mode='trilinear')
+        self.conv3 = nn.Conv3d(in_channels=256, out_channels = 1, kernel_size=1, padding=0)
+
+        #l2
+        self.deconv2 = deconvblock(256, 128) #256 + 512 -> 128
+        self.conv4 = ConvReluDrop(in_channels = 128*3, out_channels = 128, kernel_size = 3, padding = 1)
+        self.conv5 = ConvRelu(in_channels = 128, out_channels = 128, kernel_size = 3, padding = 1)
+        #l2-2
+        self.upsample2 = nn.Upsample(scale_factor=2, mode='trilinear')
+        self.conv6 = nn.Conv3d(in_channels=128, out_channels = 1, kernel_size=1, padding=0)
+
+        #l3
+        self.deconv3 = deconvblock(128, 64) # 128 + 
+        self.conv7 = ConvReluDrop(in_channels = 64*3, out_channels = 64, kernel_size = 3, padding = 1)
+        self.conv8 = ConvRelu(in_channels = 64, out_channels = 64, kernel_size = 3, padding = 1)
+        self.conv9 = nn.Conv3d(in_channels=64, out_channels = 1, kernel_size=1, padding=0)
+
+    def forward(self, x, down_sampling_feature):
+
+        #l1
+        x1 = self.deconv1(x)
+        # print('x1.shape: ', x1.shape)
+        x1 = torch.cat((x1, down_sampling_feature[-1]) , dim = 1)
+        # print('x1 concat shape: ', x1.shape)
+        x1 = self.conv1(x1)
+        x1 = self.conv2(x1)
+        #l1-2
+        x1_1 = self.upsample1(x1)
+        x1_1 = self.conv3(x1_1)
+
+        #l2
+        x2 = self.deconv2(x1)
+        x2 = torch.cat((x2, down_sampling_feature[-2]) , dim = 1)
+        # print('x2 concat shape: ', x2.shape)
+        x2 = self.conv4(x2)
+        x2 = self.conv5(x2)
+        #l2-2
+        x2_1 = self.upsample2(x2)
+        x2_1 = self.conv6(x2_1)
+
+        #l3
+        x3 = self.deconv3(x2)
+        x3 = torch.cat((x3, down_sampling_feature[-3]) , dim = 1)
+        x3 = self.conv7(x3)
+        x3 = self.conv8(x3)
+
+        x4 = self.conv9(x3)
+
+        return x1_1, x2_1, x4
+
 
 class COVID_seg(nn.Module):
     def __init__(self, in_channels = 1, out_channels = 1, final_activation = 'sigmoid'):
@@ -303,6 +376,29 @@ class COVID_seg(nn.Module):
 
         return output
     
+
+class DS_COVID_seg(nn.Module):
+    def __init__(self, in_channels = 1, out_channels = 1, final_activation = 'sigmoid'):
+        super(COVID_seg, self).__init__()
+        self.encoder = encoder(in_channels=in_channels)
+        self.decoder = decoder(out_channels=out_channels)
+
+        if final_activation == 'sigmoid':
+            self.f_activation = nn.Sigmoid()
+        else:
+            self.f_activation = nn.Softmax(dim = 1)
+
+    def forward(self, x):
+        x, d_features = self.encoder(x)
+        x1, x2, x3 = self.ds_decoder(x, d_features)
+        output = self.f_activation(x1)
+        output2 = self.f_activation(x2)
+        output3 = self.f_activation(x3)
+
+        # print("Final output shape: ", output.shape)
+
+        return output, output2, output3
+    
 # if __name__ == '__main__':
 #     inputs = torch.randn(1, 512, 16, 128, 128)
 #     # inputs = torch.randn(1, 1, 16, 128, 128)
@@ -313,15 +409,150 @@ class COVID_seg(nn.Module):
 
 #     x_test = deconv1(inputs)
 
+
+'''
+UNet
+'''
+class ConvBlock(nn.Module):
+  def __init__(self, in_channels, out_channels, kernel_size = 3, stride = 1, padding = 1):
+    super(ConvBlock, self).__init__()
+    self.conv3D = nn.Conv3d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
+    self.batch_norm = nn.BatchNorm3d(num_features=out_channels)
+
+  def forward(self, x):
+    x = self.batch_norm(self.conv3D(x))
+    x = F.relu(x)
+
+    return(x)
+  
+class Unet_Encoder(nn.Module):
+  def __init__(self, in_channels, model_depth = 4, pool_size = 2):
+    super(Unet_Encoder, self).__init__()
+    self.root_feat_maps = 16
+    self.num_conv_block = 2
+    self.module_dict = nn.ModuleDict()
+
+    for depth in range(model_depth):
+      feat_map_channels = 2**(depth+1)*self.root_feat_maps #2*16, 4*16, 8*16, 16*16
+
+      for i in range(self.num_conv_block):
+        if depth == 0:
+          self.conv_block = ConvBlock(in_channels=in_channels, out_channels=feat_map_channels) 
+          self.module_dict['conv_{}_{}'.format(depth, i)] = self.conv_block
+          in_channels, feat_map_channels = feat_map_channels, feat_map_channels*2
+        else:
+          self.conv_block = ConvBlock(in_channels=in_channels, out_channels=feat_map_channels)
+          self.module_dict['conv_{}_{}'.format(depth, i)] = self.conv_block
+          in_channels, feat_map_channels = feat_map_channels, feat_map_channels*2
+
+      if depth == model_depth - 1:  #depth = 3
+        break
+      else:
+        self.pooling = nn.MaxPool3d(kernel_size=pool_size, stride=2, padding=0)
+        self.module_dict['max_pooling_{}'.format(depth)] = self.pooling
+
+  def forward(self, x):
+    down_sampling_feature = []
+    for k, op in self.module_dict.items():
+    #   print('k: ', k, 'op: ', op)
+
+      if k.startswith('conv'):
+        # print ('input', x.shape)
+        x = op(x)
+        # print ('output', x.shape)
+        # print (k, x.shape)
+        if k.endswith('1'):
+          down_sampling_feature.append(x)
+
+      elif k.startswith('max_pooling'):
+        # print ('input', x.shape)
+        x = op(x)
+        # print ('output', x.shape)
+        # print(k, x.shape)
+
+    return x, down_sampling_feature
+
+class ConvTranspose(nn.Module):
+  def __init__(self, in_channels, out_channels, kernel_size = 3, stride = 2, padding = 1, output_padding = 1):
+    super(ConvTranspose, self).__init__()
+    self.conv3d_transpose = nn.ConvTranspose3d(in_channels=in_channels, out_channels=out_channels,kernel_size=kernel_size,
+    stride=stride, padding=padding, output_padding = output_padding)
+
+  def forward(self, x):
+    return(self.conv3d_transpose(x))
+
+class Unet_Decoder(nn.Module):
+  def __init__(self, out_channels, model_depth = 4):
+    super(Unet_Decoder, self).__init__()
+    self.num_conv_blocks = 2
+    self.num_feat_maps = 16
+    self.module_dict = nn.ModuleDict()
+
+    for depth in range(model_depth-2, -1, -1): #2, 1, 0
+      feat_map_channels = 2**(depth+1)*self.num_feat_maps #128, 64, 32
+      self.deconv = ConvTranspose(in_channels=feat_map_channels*4, out_channels=feat_map_channels*4) #512, 256, 128
+      self.module_dict['deconv_{}'.format(depth)] = self.deconv
+      for i in range(self.num_conv_blocks):
+        if i == 0:
+          self.conv = ConvBlock(in_channels=feat_map_channels*6, out_channels=feat_map_channels*2) #768/256, 384/128, 192/64
+          self.module_dict['conv_{}_{}'.format(depth, i)] = self.conv
+        else:
+          self.conv = ConvBlock(in_channels=feat_map_channels*2, out_channels=feat_map_channels*2) #256/256, 128/128, 64/64
+          self.module_dict['conv_{}_{}'.format(depth, i)] = self.conv
+      if depth == 0:
+        self.final_conv = ConvBlock(in_channels=feat_map_channels*2, out_channels=out_channels)
+        self.module_dict['final_conv'] = self.final_conv
+
+  def forward(self, x, down_sampling_feature):
+    for k, op in self.module_dict.items():
+      if k.startswith('deconv'):
+        x = op(x)
+        # print ('operation: ',k, 'output shape: ', x.shape) 
+        x = torch.cat((down_sampling_feature[int(k[-1])], x), dim = 1)
+        # print ('concat shape1', down_sampling_feature[int(k[-1])].shape) 
+        # print ('operation: concat', 'output shape: ', x.shape) 
+      elif k.startswith('conv'):
+        x = op(x)
+        # print ('operation: ',k, 'output shape: ', x.shape) 
+      else:
+        x = op(x)
+        # print ('operation: ',k, 'output shape: ', x.shape) 
+    
+    return x
+
+
+class Unet3D(nn.Module):
+    def __init__(self, in_channels = 1, out_channels = 1, model_depth = 4, final_activation = 'sigmoid'):
+        super(Unet3D, self).__init__()
+        self.encoder = Unet_Encoder(in_channels=in_channels, model_depth=model_depth)
+        self.decoder = Unet_Decoder(out_channels=out_channels, model_depth=model_depth)
+        if final_activation == 'sigmoid':
+            self.f_activation = nn.Sigmoid()
+        else:
+            self.f_activation = nn.Softmax(dim = 1)
+
+    def forward(self, x):
+        x, d_features = self.encoder(x)
+        x = self.decoder(x, d_features)
+        x = self.f_activation(x)
+        # print("Final output shape: ", x.shape)
+
+        return x
+    
+###Main 
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 "cuda" if torch.cuda.is_available() else "cpu"
 
+
 if __name__ == '__main__':
     # inputs = torch.randn(1, 512, 16, 128, 128)
-    inputs = torch.randn(1, 1, 16, 128, 128)
+    inputs = torch.randn(1, 1, 8, 128, 128)
     inputs = inputs.cuda()
 
-    model = COVID_seg(1, 1)
+    model = COVID_seg()
+    model = DS_COVID_seg()
+    model = Unet3D()
     model.cuda()
 
     x_test = model(inputs)
